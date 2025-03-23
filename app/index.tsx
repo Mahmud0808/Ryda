5e625531075264ad02576e1a9086a356f2d2a8ee
DrDisagree <29881338+Mahmud0808@users.noreply.github.com>
2025-03-24 01:36:45 +0600

    Redirect to homepage if logged in

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,12 @@
+import { useAuth } from "@clerk/clerk-expo";
 import { Redirect } from "expo-router";
-import React from "react";
 
-const Home = () => {
-  return <Redirect href={"/(auth)/onboarding"} />;
+const Page = () => {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) return <Redirect href="/(root)/(tabs)/home" />;
+
+  return <Redirect href="/(auth)/onboarding" />;
 };
 
-export default Home;
+export default Page;
